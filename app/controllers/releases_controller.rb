5f3cf4387b9b1f7e6e109afa49a81160233365e8
snugthejoiner <joiner@jedparish.com>
2015-06-30 18:46:31 -0400

    added releases

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,22 +1,54 @@
 class ReleasesController < ApplicationController
+  before_action
+
   def index
+    @releases = Release.all
   end
 
   def new
+    @release = Release.new
   end
 
   def create
+    @release = Release.new(release_params)
+
+    if @release.save
+      flash[:notice] = "Release was created."
+      redirect_to @release
+    else
+      flash[:error] = "There was an error saving the release."
+      render :new
+    end
   end
 
   def edit
+    @release = Release.find(params[:id])
   end
 
   def destroy
+    @release.destroy
   end
 
   def show
+    @release = Release.find(params[:id])
   end
 
   def update
+    @release = Release.find(params[:id])
+    if @release.update(song_params)
+      flash[:notice] = "This release has been updated."
+      redirect_to @release
+    else
+      flash[:error] = "There was an error updating the release."
+      render :edit
+    end
   end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def release_params
+    params.require(:release).permit(:title, :release_date)
+  end
+
 end
