5d001bf3e525715cb8bc43057e1f0cd0b8680caa
snugthejoiner <joiner@jedparish.com>
2015-10-10 17:28:34 -0400

    new user email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
  
         @user = user
 
-        mail(to: jed@jedparish.com, subject: "New user #{user.name}")
+        mail(to: "jed@jedparish.com", subject: "New user #{@user.name}")
     end
 end
